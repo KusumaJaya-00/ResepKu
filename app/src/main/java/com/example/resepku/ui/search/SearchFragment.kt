cda87533a56500e0312d6ac74d4ea6c56e6b483a
KusumaJaya-00 <240040100@stikom-bali.ac.id>
2026-06-23 22:02:06 +0800

    menambah manifest, template Fragment kosong, menu bottom nav, dan drawable ikon untuk setup awal navigasi

--- app/src/main/java/com/example/resepku/ui/search/SearchFragment.kt
+++ app/src/main/java/com/example/resepku/ui/search/SearchFragment.kt
@@ -1,6 +1,6 @@
 package com.example.resepku.ui.search
 
 import androidx.fragment.app.Fragment
+import com.example.resepku.R
 
-class SearchFragment : Fragment(){
-}
+class SearchFragment : Fragment(R.layout.fragment_search)
