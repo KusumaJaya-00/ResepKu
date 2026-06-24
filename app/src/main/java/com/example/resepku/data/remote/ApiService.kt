8656313f165a5716a8f3a5a57bb0128d88b177ae
I Made Kusuma Jaya Wardana <240040100@stikom-bali.ac.id>
2026-06-24 18:29:06 +0800

    Revert "menambahkan RetrofitClient, ApiService ke dalam project"

--- app/src/main/java/com/example/resepku/data/remote/ApiService.kt
+++ app/src/main/java/com/example/resepku/data/remote/ApiService.kt
@@ -1,9 +1,4 @@
 package com.example.resepku.data.remote
 
-import retrofit2.Call
-import retrofit2.http.GET
-
 interface ApiService {
-    @GET("resep.json")
-    fun getResep(): Call<List<Resep>>
 }
